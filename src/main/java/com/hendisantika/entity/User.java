c54d34b9912dd1cdfece8ad379828fb9bae79423
irpanibnusolih <irfanyoungest11@gmail.com>
2022-04-28 22:10:33 +0700

    edit database

--- src/main/java/com/hendisantika/entity/User.java
+++ src/main/java/com/hendisantika/entity/User.java
@@ -23,7 +23,7 @@ import java.util.Set;
  */
 @Data
 @Entity
-@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(uniqueConstraints = @UniqueConstraint(columnNames = "email"),name="t_user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
